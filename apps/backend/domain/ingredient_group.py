3f0cf1d71545035d5c417019f9ade14b646b992a
= <=>
2026-06-25 20:19:13 +0900

    modify nutrition data

--- apps/backend/domain/ingredient_group.py
+++ apps/backend/domain/ingredient_group.py
@@ -1,8 +1,10 @@
 class IngredientGroup:
     def __init__(
         self,
+        nutrition_id: int,
         group_id: int,
         group_name: str,
     ) -> None:
+        self.nutrition_id = nutrition_id
         self.group_id = group_id
         self.group_name = group_name
